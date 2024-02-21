1635d46380817d2f1fce99888ce57bf2dfd4a162
Grant Campanelli <grantcampanelli@gmail.com>
2024-02-21 13:14:51 -0800

    kinda working

--- pages/stashes/[id].tsx
+++ pages/stashes/[id].tsx
@@ -7,10 +7,16 @@ import { GetServerSideProps } from "next";
 import prisma from "../../lib/prismadb";
 // import type { Stash } from "@prisma/client";
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  res,
+  query,
+}) => {
   const session = await getServerSession(req, res, authOptions);
   //   const session = await getSession({ req });
 
+  console.log("id query parm: ", query);
+  let stashId: string = Array.isArray(query.id) ? "" : query.id || "";
   if (!session) {
     res.statusCode = 403;
     console.log("checking session and not finding it in stashes:", session);
@@ -24,32 +30,46 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     };
   }
 
-  const stashes = await prisma.stash.findMany({
+  const stash = await prisma.stash.findUnique({
     where: {
-      userId: session?.user?.id,
+      id: stashId,
+    },
+    include: {
+      shelves: {
+        include: {
+          shelfItems: {
+            include: {
+              bottle: true,
+            },
+          },
+        },
+      },
     },
   });
+
+  console.log("stash: ", stash);
+
   return {
-    props: { stashes },
+    props: { stash },
   };
 };
 
 type Props = {
-  stashes: Stash[];
+  stash: Stash;
 };
 
 const Stashes: React.FC<Props> = (props) => {
-
   return (
     <>
       <div className="flex justify-center">
         <h1>My Stashes</h1>
       </div>
       <div className="grid grid-cols-4 gap-4 place-items-center">
-        {props.stashes.map((stash) => (
-          <div key={stash.id}>
+        <h1>{props.stash.name}</h1>
+        {props.stash.shelves.map((shelf) => (
+          <div key={shelf.id}>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-              {stash.name}
+              {shelf.name}
             </button>
           </div>
         ))}
