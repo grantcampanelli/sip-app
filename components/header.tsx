95b318a7a30d2608718107d97b345cbf19105c39
Grant Campanelli <grantcampanelli@gmail.com>
2024-02-21 08:19:55 -0800

    Fixing routing

--- components/header.tsx
+++ components/header.tsx
@@ -3,65 +3,55 @@ import { useSession, getSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 
-const Header = () => {
+const SignedIn = () => {
   const { data: session } = useSession();
   if (session) {
     return (
-      <header className="bg-sky-100">
-        <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex items-center text-gray-900 hover:text-gray-900">
-            <Link href="/">
-              <Image
-                src="/SipLogoNoTag.png"
-                width={100}
-                height={100}
-                alt="Sip Logo"
-              />
-            </Link>
-          </div>
-          <div className="flex items-center">
-            <a
-              href="/stashes"
-              className="ml-8 text-gray-900 hover:text-gray-900"
-            >
-              Stashes
-            </a>
-            <a className="ml-8 text-gray-900 hover:text-gray-900">
-              <button onClick={() => signOut()}>Sign Out</button>
-            </a>
-          </div>
-        </nav>
-      </header>
+      <>
+        <div className="flex items-center">
+          <a href="/stashes" className="ml-8 text-gray-900 hover:text-gray-900">
+            Stashes
+          </a>
+
+          <a className="ml-8 text-gray-900 hover:text-gray-900">
+            <button onClick={() => signOut()}>Sign Out</button>
+          </a>
+        </div>
+      </>
     );
   } else {
     return (
-      <header className="bg-sky-100">
-        <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex items-center text-gray-900 hover:text-gray-900">
-            <Link href="/">
-              <Image
-                src="/SipLogoNoTag.png"
-                width={100}
-                height={100}
-                alt="Sip Logo"
-              />
-            </Link>
-          </div>
-          <div className="flex items-center">
-            <a className="ml-8 text-gray-900 hover:text-gray-900">
-              <button onClick={() => signIn()}>Sign in</button>
-            </a>
-          </div>
-        </nav>
-      </header>
+      <>
+        <div className="flex items-center">
+          <a className="ml-8 text-gray-900 hover:text-gray-900">
+            <button onClick={() => signIn()}>Sign in</button>
+          </a>
+        </div>
+      </>
     );
   }
+};
+const Header = () => {
+  return (
+    <header className="bg-sky-100">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex items-center text-gray-900 hover:text-gray-900">
+          <Link href="/">
+            <Image
+              src="/SipLogoNoTag.png"
+              width={100}
+              height={100}
+              alt="Sip Logo"
+            />
+          </Link>
+        </div>
+        <SignedIn />
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,41 @@ import Image from "next/image";
 export default function Page() {
   const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <div className="container">
-        <p>You're signed in</p>
-      </div>
-    );
-  }
+  // if (session) {
+  //   return (
+  //     <div className="container">
+  //       <p>You're signed in</p>
+  //     </div>
+  //   );
+  // }
   return (
-    <div className="container">
-      <Image src="/SipLogo.png" width={500} height={500} alt="Sip Logo" />
+    <div>
+      <div className="grid grid-cols-3 justify-center">
+        <div>
+          <Image
+            src="/images/beer-fridge-animated.png"
+            alt="Beer Fridge"
+            width={800}
+            height={800}
+          />
+        </div>
+        <div>
+          <Image
+            src="/images/whiskey-cabinet-animated.png"
+            alt="Beer Fridge"
+            width={800}
+            height={800}
+          />
+        </div>
+        <div className="justify-center">
+          <Image
+            src="/images/wine-fridge-animated.png"
+            alt="Beer Fridge"
+            width={800}
+            height={800}
+          />
+        </div>
+      </div>
     </div>
   );
 }
