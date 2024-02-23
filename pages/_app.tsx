a52920214244f44b8f97462c1cad3caf2b0cca2c
Grant Campanelli <grantcampanelli@gmail.com>
2024-02-23 13:51:22 -0800

    removing tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Header from "../components/header";
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
+import "@mantine/core/styles.css";
 
 // * NEXTAUTH - import SessionProvider
 import { SessionProvider } from "next-auth/react";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 
 export default function App({
   Component,
@@ -18,12 +20,15 @@ export default function App({
         <meta name="description" content="Know what you're sippin'" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <ColorSchemeScript />
       </Head>
 
       {/* NOTE: NEXTAUTH - wrap the app with SessionProvider */}
       <SessionProvider session={session}>
-        <Header />
-        <Component {...pageProps} />
+        <MantineProvider>
+          <Header />
+          <Component {...pageProps} />
+        </MantineProvider>
       </SessionProvider>
     </>
   );
