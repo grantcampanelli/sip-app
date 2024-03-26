d3a1f71b2bc738610db5f4e4976db66c6b7f99e7
Grant Campanelli <grantcampanelli@gmail.com>
2024-03-25 22:26:53 -0700

    Tables cleaned up

--- pages/brands/[id]/index.tsx
+++ pages/brands/[id]/index.tsx
@@ -17,15 +17,36 @@ import {
   Box,
   TextInput,
   NumberInput,
+  Table,
+  ScrollArea,
+  UnstyledButton,
+  Text,
+  Center,
+  rem,
+  keys,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useForm } from "@mantine/form";
+import classes from "/styles/TableSort.module.css";
+import { useState } from "react";
+import {
+  IconSelector,
+  IconChevronDown,
+  IconChevronUp,
+  IconSearch,
+} from "@tabler/icons-react";
 
 type BrandWithFullData = Prisma.BrandGetPayload<{
   include: {
     products: true;
   };
 }>;
+
+type ProductFlatRow = {
+  id: string;
+  name: string;
+  vintage: string;
+};
 
 export const getServerSideProps: GetServerSideProps = async ({
   req,
@@ -56,15 +77,22 @@ export const getServerSideProps: GetServerSideProps = async ({
       },
     })) || null;
 
+  const productFlatRowData: ProductFlatRow[] =
+    brand?.products.map((brand) => {
+      return { id: brand.id, name: brand.name, vintage: brand.vintage };
+    }) || [];
+
   return {
     props: {
       brand,
+      productFlatRowData,
     },
   };
 };
 
 type Props = {
   brand: BrandWithFullData;
+  productFlatRowData: ProductFlatRow[];
 };
 
 const Brand: React.FC<Props> = (props) => {
@@ -100,6 +128,114 @@ const Brand: React.FC<Props> = (props) => {
     }
   };
 
+  /*
+   *  Table Code
+   */
+
+  interface ProductRowData {
+    id: string;
+    name: string;
+    vintage: string;
+  }
+
+  interface ThProps {
+    children: React.ReactNode;
+    reversed: boolean;
+    sorted: boolean;
+    onSort(): void;
+  }
+
+  function Th({ children, reversed, sorted, onSort }: ThProps) {
+    const Icon = sorted
+      ? reversed
+        ? IconChevronUp
+        : IconChevronDown
+      : IconSelector;
+    return (
+      <Table.Th className={classes.th}>
+        <UnstyledButton onClick={onSort} className={classes.control}>
+          <Group justify="space-between">
+            <Text fw={500} fz="sm">
+              {children}
+            </Text>
+            <Center className={classes.icon}>
+              <Icon style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+            </Center>
+          </Group>
+        </UnstyledButton>
+      </Table.Th>
+    );
+  }
+  function filterData(data: ProductRowData[], search: string) {
+    const query = search.toLowerCase().trim();
+    return data.filter((item) =>
+      keys(data[0]).some((key) => item[key].toLowerCase().includes(query))
+    );
+  }
+
+  function sortData(
+    data: ProductRowData[],
+    payload: {
+      sortBy: keyof ProductRowData | null;
+      reversed: boolean;
+      search: string;
+    }
+  ) {
+    const { sortBy } = payload;
+
+    if (!sortBy) {
+      return filterData(data, payload.search);
+    }
+
+    return filterData(
+      [...data].sort((a, b) => {
+        if (payload.reversed) {
+          return b[sortBy].localeCompare(a[sortBy]);
+        }
+
+        return a[sortBy].localeCompare(b[sortBy]);
+      }),
+      payload.search
+    );
+  }
+  const [search, setSearch] = useState("");
+  const [sortedData, setSortedData] = useState(props.productFlatRowData);
+  const [sortBy, setSortBy] = useState<keyof ProductRowData | null>(null);
+  const [reverseSortDirection, setReverseSortDirection] = useState(false);
+
+  const setSorting = (field: keyof ProductRowData) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false;
+    setReverseSortDirection(reversed);
+    setSortBy(field);
+    setSortedData(
+      sortData(props.productFlatRowData, { sortBy: field, reversed, search })
+    );
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setSearch(value);
+    setSortedData(
+      sortData(props.productFlatRowData, {
+        sortBy,
+        reversed: reverseSortDirection,
+        search: value,
+      })
+    );
+  };
+
+  const rows = sortedData.map((row) => (
+    <Table.Tr key={row.name}>
+      <Table.Td>{row.name}</Table.Td>
+      <Table.Td>{row.vintage}</Table.Td>
+      <Table.Td>
+        <Link href={`/products/${row.id}`}>
+          <Button>View</Button>
+        </Link>
+      </Table.Td>
+    </Table.Tr>
+  ));
+
   return (
     <Container>
       <Group justify="space-between" h="100%" pl="10px" pt="10px">
@@ -108,7 +244,7 @@ const Brand: React.FC<Props> = (props) => {
         <Button onClick={open}>Add Product</Button>
       </Group>
 
-      {props.brand.products.map((product, index) => (
+      {/* {props.brand.products.map((product, index) => (
         <Link
           style={{ textDecoration: "none" }}
           href={`/products/${product.id}`}
@@ -118,7 +254,69 @@ const Brand: React.FC<Props> = (props) => {
             {product.vintage} {product.name}
           </Button>
         </Link>
-      ))}
+      ))} */}
+
+      <ScrollArea>
+        <TextInput
+          placeholder="Search by any field"
+          mb="md"
+          leftSection={
+            <IconSearch
+              style={{ width: rem(16), height: rem(16) }}
+              stroke={1.5}
+            />
+          }
+          value={search}
+          onChange={handleSearchChange}
+        />
+        <Table
+          horizontalSpacing="md"
+          verticalSpacing="xs"
+          miw={700}
+          layout="fixed"
+        >
+          <Table.Tbody>
+            <Table.Tr>
+              <Th
+                sorted={sortBy === "name"}
+                reversed={reverseSortDirection}
+                onSort={() => setSorting("name")}
+              >
+                Name
+              </Th>
+              <Th
+                sorted={sortBy === "vintage"}
+                reversed={reverseSortDirection}
+                onSort={() => setSorting("vintage")}
+              >
+                Vintage
+              </Th>
+              <Th
+                sorted={sortBy === "id"}
+                reversed={reverseSortDirection}
+                onSort={() => setSorting("id")}
+              >
+                Link
+              </Th>
+            </Table.Tr>
+          </Table.Tbody>
+          <Table.Tbody>
+            {rows.length > 0 ? (
+              rows
+            ) : (
+              <Table.Tr>
+                <Table.Td
+                  colSpan={Object.keys(props.productFlatRowData[0]).length}
+                >
+                  <Text fw={500} ta="center">
+                    Nothing found
+                  </Text>
+                </Table.Td>
+              </Table.Tr>
+            )}
+          </Table.Tbody>
+        </Table>
+      </ScrollArea>
 
       <Modal opened={opened} onClose={close} title="Add Product">
         <Box maw={340} mx="auto">
